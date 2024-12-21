295ee6824ef7bdaa734f81060279f9eba11b9ce4
unmy040915 <m.yuuta0915@icloud.com>
2024-12-21 16:27:40 +0900

    時間の表示が成功した

--- Clockdesign/ContentView.swift
+++ Clockdesign/ContentView.swift
@@ -8,17 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+     @State private var currentTime: String = ""
+    let timeFormatter: DateFormatter = {
+        let formatter = DateFormatter() //DateFormatter()は型の名前？？
+            formatter.dateFormat = "HH:mm:ss"
+            return formatter
+    }()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            ZStack {
+                let SelectColor = Color.blue
+                SelectColor.ignoresSafeArea()
+                HStack{
+                    let timeComponents = currentTime.split(separator: ":")//:のあるところでフォーマットを分割
+                    if timeComponents.count == 3 {//Listみたいにして，HH,mm,ssを0,1,2にするssだけfontsizeを変更
+                        HStack(alignment: .lastTextBaseline){
+                            Text("\(timeComponents[0]):\(timeComponents[1])")
+                                .font(.custom("Menlo", size: 220))
+                            Text("\(timeComponents[2])")
+                                .font(.custom("Menlo", size: 110)) // 秒だけ小さくする
+                        }
+                    }
+                }
+                    }
+            .onAppear {//時間の更新
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                    currentTime = timeFormatter.string(from: Date())
+                }
+            }
+        
     }
+    
 }
 
-#Preview {
-    ContentView()
-}
